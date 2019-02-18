20ff01ab035c219781de7d1803d6fa91217a7479
ClaireLC <clairech@stanford.edu>
2019-02-17 20:38:45 -0800

    changed jr file path

--- robosuite/models/robots/jr2_robot.py
+++ robosuite/models/robots/jr2_robot.py
@@ -7,7 +7,7 @@ class JR2(Robot):
     """JR2."""
 
     def __init__(self):
-        super().__init__(xml_path_completion("robots/JR2/jr2_with_arm.xml"))
+        super().__init__(xml_path_completion("robots/jr2/jr2_with_arm.xml"))
 
         self.bottom_offset = np.array([0, 0, 0])
 
