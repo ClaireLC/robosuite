8fa3bf5a3b93825dceaefdcbc5c2305d5fbbc408
ClaireLC <clairech@stanford.edu>
2019-02-22 00:56:02 -0800

    custom parser

---
+++ robosuite/scripts/custom_parser.py
@@ -0,0 +1,109 @@
+import argparse
+import json
+import os
+from gym import logger
+
+def custom_arg_parser():
+  parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+  # try to load default config file
+  default_path = './default_door.json'
+  parser.add_argument("--config_file", help=".json file to load parameters from", default=default_path)
+ 
+  parser.add_argument("--model", help="Directory containing a previously trained model", type=str)
+  parser.add_argument("--visualize", help="Visualize the training process", type=str2bool, const=True, nargs='?')
+  parser.add_argument("--replay", help="Replay policy of given model, without modifying", type=str2bool, const=True, nargs='?')
+  parser.add_argument("--stochastic_replay", help="Whether or not to load a stochastic model", type=str2bool, const=True, nargs='?')
+  parser.add_argument("--slurm", help="Whether or not you are training on cluster", type=str2bool, const=True, nargs='?')
+
+  # Environment parameters
+  parser.add_argument("--bot_motion", help="Type of robot motion (static or mobile base)", type=str, choices=['static','mmp'])
+  parser.add_argument("--door_type", help="Door type to use", type=str, choices=['dpnl','dpwl'])
+  parser.add_argument("--arena", help="Arena type, either empty or room", type=str, choices=['e','r'])
+  parser.add_argument("--distance", help="Distance from robot to door", type=str, choices=['touching','near','far'])
+  parser.add_argument("--robot_pos", help="Position of door [x,y,z]", nargs='+', type=float)
+  parser.add_argument("--door_pos", help="Position of door [x,y,z]", nargs='+', type=float)
+  parser.add_argument("--door_quat", help="Position of door [w,x,y,z]", nargs='+', type=float)
+  parser.add_argument("--control_freq", help="Control frequency", type=int)
+
+  # Learning parameters
+  parser.add_argument("--rl_alg", help="RL algorithm to use", type=str, choices=['ppo1','ppo2'])
+  parser.add_argument("--policy", help="Policy model to use", type=str)
+  parser.add_argument("--n_steps", help="Number of steps to run for each environment per update", type=int)
+  parser.add_argument("--horizon", help="Number of steps before environment resets", type=int)
+  parser.add_argument("--opt_epochs", help="Number of epochs when optimizing surrogate", type=int)
+  parser.add_argument("--minibatches", help="Number of training minibatches per update", type=int)
+  parser.add_argument("--ent_coef", help="Entropy coefficient for loss", type=float)
+  parser.add_argument("--vf_coef", help="Value function coefficient for loss", type=float)
+  parser.add_argument("--clip_range", help="Clipping parameter", type=float)
+  parser.add_argument("--gamma", help="Discount factor", type=float)
+  parser.add_argument("--lam", help="Factor for trade-off of bias vs variance for GAE", type=float)
+  parser.add_argument("--verbose", help="Verbosity level", type=int)
+  parser.add_argument("--total_timesteps", help="Total timesteps to train", type=int)  
+  parser.add_argument("--n_cpu", help="Number of CPUs to train on", type=int)  
+
+  return parser
+
+def str2bool(v):
+  if v.lower() in ('yes', 'true', 't', 'y'):
+    return True
+  elif v.lower() in ('no', 'false', 'f', 'n'):
+    return False
+  else:
+    raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def serialize_args(args):
+  ret = vars(args)["distance"]
+  for key, value in vars(args).items():
+    # skip config file etc. path names
+    if type(value) == str:
+      if '/' in value: continue
+      if 'log_suffix' == key: continue
+      if 'log_dir' == key: continue
+
+    if value is None: continue
+    
+    if key == "total_timesteps": continue
+    if key == "visualize": continue
+    if key == "verbose": continue
+    if key == "distance": continue
+
+    # Necessary to deal with length
+    splits = key.split('_')
+    short_key = ""
+
+    for split in splits:
+      short_key += split[0]
+    ret += "-{}.{}".format(short_key, value)
+  if ret != "" and ret[-1] == '_':
+    ret = ret[:-1]
+  return ret.replace(" ", "")
+
+def load_defaults(args):
+  default_path = './default_door.json'
+  default_config_file = default_path
+  specified_config_file = args.config_file
+
+  updateable_args_dict = vars(args)
+  
+  # Load the config file, if there is one specified
+  if args.config_file is not None:
+    with open(specified_config_file) as f:
+      specified_args = json.load(f)
+      for key in updateable_args_dict.keys():
+        if key in specified_args:
+          if updateable_args_dict[key] is None:
+            updateable_args_dict[key] = specified_args.get(key)      
+        else:
+          print("Argument " + key + " not specified in config file")
+
+  # Fill in all the None (unspecified)  args from default config file 
+  with open(default_config_file) as def_f:
+    default_args = json.load(def_f)
+    for key in updateable_args_dict.keys():
+      if key in default_args:
+        if updateable_args_dict[key] is None:
+          updateable_args_dict[key] = default_args.get(key)      
+      else:
+        print("Argument " + key + " not defined")
+
--- robosuite/scripts/learn_door.py
+++ robosuite/scripts/learn_door.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import numpy as np
 import time
+import json
 
 import robosuite as suite
 from stable_baselines.common.policies import MlpPolicy,MlpLstmPolicy,MlpLnLstmPolicy
@@ -10,58 +11,92 @@ from stable_baselines import PPO1
 from stable_baselines import PPO2
 
 from robosuite.wrappers import GymWrapper
+from robosuite.scripts.custom_parser import custom_arg_parser, load_defaults, serialize_args
 
-if __name__ == "__main__":
-  # Parameters
-  rlalg           = "ppo2"
-  door            = "dpnl"
-  arena           = "e"
-  setting         = "eef_over_handle"
-  gamma           = 0.99
-  nsteps          = 2048
-  horizon         = 1025
-  optepochs       = 15
-  minibatches     = 32
-  ent_coef        = 0.0
-  vf_coef         = 0.5
-  cliprange       = 0.2
-  lamda           = 0.95
-  total_timesteps = 5000000
+def main():
 
+  parser = custom_arg_parser()
+  args = parser.parse_args()
+  load_defaults(args)
+  print(args)
+  print(serialize_args(args))
   # Create the model name with all the parameters
   
-  model_name = 
+  model_name = serialize_args(args)
   model_save_path = "../../learned_models/" + model_name + "/"
   tb_save_path = "../../tb_logs/" +  model_name + "/"
-  final_model_path = model_save_path + "final_" + model_name
-  show_render = False
+  final_model_path = model_save_path + "final-" + model_name
+  show_render = args.visualize
 
   env = GymWrapper(
       suite.make(
       "JR2Door",
       has_renderer=show_render,
       use_camera_obs=False,
-      horizon=1024,
       ignore_done=False,
-      control_freq=20
+      control_freq=args.control_freq,
+      horizon=args.horizon,
+      door_type=args.door_type,
+      arena=args.arena,
+      bot_motion=args.bot_motion,
+      robot_pos=args.robot_pos
     )
   )
   
-  print(env.__dict__)
-  #env = DummyVecEnv([lambda: env])
-  env = SubprocVecEnv([lambda: env for i in range(4)])
+  if args.slurm:
+    env = SubprocVecEnv([lambda: env for i in range(args.n_cpu)])
+  else:
+    env = DummyVecEnv([lambda: env])
 
- # model = PPO1(MlpPolicy,env, verbose=1,timesteps_per_actorbatch=2000, optim_epochs=10,tensorboard_log=tb_save_path)
-  model = PPO2(MlpPolicy, env, verbose=1,n_steps=nsteps, nminibatches=minibatches, noptepochs=optepochs,cliprange=cliprange,ent_coef=ent_coef,tensorboard_log=tb_save_path)
+  # Load the specified model, if there is one
+  if args.model is not None:
+    # Training from checkpoint, so need to reset timesteps for tb
+    reset_num_timesteps = False
+    if args.rl_alg == "ppo2":
+      model = PPO2.load(args.model,env=env)
+    if args.rl_alg == "ppo1":
+      model = PPO1.load(args.model,env=env)
+  else: 
+    # New model, so need to reset timesteps for tb
+    reset_num_timesteps = True
+    if args.rl_alg == "ppo2":
+      model = PPO2(
+                  args.policy,
+                  env,
+                  verbose=args.verbose,
+                  n_steps=args.n_steps,
+                  nminibatches=args.minibatches,
+                  noptepochs=args.opt_epochs,
+                  cliprange=args.clip_range,
+                  ent_coef=args.ent_coef,
+                  tensorboard_log=tb_save_path
+                  )
 
-  model.learn(total_timesteps = total_timesteps,save_dir = model_save_path, render=show_render)
-  model.save(final_model_path)
-  #env.render()
-  #print(env.sim.data.qpos[env._ref_joint_vel_indexes])
-  #model = PPO1.load("models/8192model.ckpt")
+    elif args.rl_alg == "ppo1":
+      model = PPO1(
+                  args.policy,
+                  env,
+                  verbose=args.verbose,
+                  timesteps_per_actorbatch=args.n_steps,
+                  optim_epochs=args.opt_epochs,
+                  tensorboard_log=tb_save_path,
+                  )
+  if args.replay:
+    quit()
+    # Replay a policy
+  else:
+    # Train
+    model.learn(
+                total_timesteps = args.total_timesteps,
+                save_dir = model_save_path,
+                render=show_render,
+                reset_num_timesteps=reset_num_timesteps,
+                )
+
+    model.save(final_model_path)
   
-  print("Done training")
-  obs = env.reset()
+    print("Done training")
+    obs = env.reset()
   quit()
   while True:
     action, _states = model.predict(obs)
@@ -70,3 +105,7 @@ if __name__ == "__main__":
     #print(obs)
     #print(env.sim.data.qpos[env._ref_joint_vel_indexes])
     #time.sleep(0.1)
+
+if __name__ == "__main__":
+  main()
+
